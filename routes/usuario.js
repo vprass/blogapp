e9be0b1526538fd9e6e87305016193a23ae0276a
Vinícius Prass <vinicius_prass@hotmail.com>
2023-07-24 22:17:22 -0300

    emplementando autenticação

--- routes/usuario.js
+++ routes/usuario.js
@@ -81,4 +81,12 @@ router.post('/registro', (req, res) => {
     }
 })
 
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/usuarios/login',
+        failureFlash: true
+    })(req, res, next)
+})
+
 module.exports = router;
